295e0f6fe0a71e0bb1dfa660970d28a257bd0eba
Aurelien Montmejat <aurelien.montmejat@gmail.com>
2018-12-19 14:49:02 +0100

    Ajout des structures descripteurs et pile

--- Commun/descripteur.c
+++ Commun/descripteur.c
@@ -1,6 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "descripteur.h"
 
-void init_DESCR(DESCR* descriptor)
+void init_DESCR(DESCR* descriptor, int id)
 {
     descriptor->lastWordIndex = 0;
+    descriptor->id = id;
+}
+
+void fill_DESCR(DESCR descriptor, FILE* file)
+{
+    int id, termes, total;
+    size_t len = 0;
+    char* line = NULL;
+
+    while (getline(&line, &len, file) != EOF)
+    {
+        fscanf(file, "%d %d %d\n", &id, &termes, &total);
+        printf("%d %d %d\n", id, termes, total);
+    }
 }
