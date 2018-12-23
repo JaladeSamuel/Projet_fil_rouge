088e4003f369c9fa52047c54c84966795665add4
Thomas <lorigny.thomas@gmail.com>
2018-12-23 17:37:50 +0100

    Descripteur image NB et RGB

---
+++ IndexationImage/testimage.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include "image.h"
+
+
+int main(void) {
+  printf("Hello World\n");
+  
+  //descripteurNB();
+  
+  descripteurRGB();
+  printf("\nGoodbye World\n");
+  return 0;
+}
