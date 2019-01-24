febdd13bf0504fc68764198feacc902e6d83f0d3
JaladeSamuel <sam34440@hotmail.fr>
2019-01-24 17:28:35 +0100

    "config"

--- Menu/menu1.c
+++ Menu/menu1.c
@@ -7,18 +7,18 @@
 void quitter(){
 	printf("\nFin du programme.\n");
 	exit(EXIT_SUCCESS);
-	
+
 }
 
 void identification(){
 	system("stty -echo; read -p \"\nMot de passe : \" password; stty echo; while [ $password != \"123upssitech\" ] ;do echo \"\nMot de passe refusé ! Réessayez.\n\";stty -echo; read -p \"\nMot de passe : \" password; stty echo; done; echo \"\nMot de passe accepté.\"");
-	
+
 }
 
 void menu1(){
 	int mode=0;
 	char saisie[255];
-	
+
 	while(1)
 	{
 		printf("\nSélectionner un mode : \n1 - Utilisateur\n2 - Administrateur\nType de connexion : ");
@@ -27,7 +27,7 @@ void menu1(){
 		printf("\nErreur de saisie, veuillez saisir un entier.\n");
 	}
 
-	switch(mode){	
+	switch(mode){
 	case 1 :
 		menu2();
 		break;
@@ -35,7 +35,7 @@ void menu1(){
 		identification();
 		choix_adm();
 		break;
-	default : 
+	default :
 		printf("\nErreur : Saisie impossible, veuillez choisir \"1\" ou \"2\".\n");
 		menu1();
 		break;
@@ -52,35 +52,10 @@ void rechercheParAdresse(){
 	RESULTS res;
 	init_RES(&res);
 
-	printf("\nRentrer le nom du fichier : ");
+	printf("\nRentrer le nom du fichier present dans la base que vous voules comparer ou un chemin absolu : ");
 	scanf("%s", path);
 
 	searchFILE_COMPTXT(path, &res);
 
 	print_RES(res);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
