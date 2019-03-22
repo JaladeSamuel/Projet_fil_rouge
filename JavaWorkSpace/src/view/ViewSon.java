d4982187f9d9b481f41243bce94d11027224bd1c
Thomas <lorigny.thomas@gmail.com>
2019-03-22 15:06:57 +0100

    ViewSon

--- JavaWorkSpace/src/view/ViewSon.java
+++ JavaWorkSpace/src/view/ViewSon.java
@@ -1,11 +1,49 @@
 package view;
 
+//import controller.truc;
+
 public class ViewSon implements runnableView {
+
+    private ViewUtilisateur viewUtilisateur;
+    //private ViewRechercheCouleur viewRechercheCouleur;
+    //private ViewRechercheNoirBlanc rechercheNoirBlanc;
+
+    private String accueilViewSon = "Sélectionner un type d'image :\n"+
+            "1 - Noir et Blanc\n"+
+            "2 - Couleur\n"+
+            "3 - Retour\n"+
+            "Type de recherche : ";
+    private int choix;
+
     public ViewSon(ViewUtilisateur viewUtilisateur) {
+        this.viewUtilisateur = viewUtilisateur;
+    }
+    private void init() {
+        choix = -1;
     }
 
     @Override
     public void run() {
-
+        init();
+        do {
+            System.out.println(accueilViewSon);
+            System.out.println("\nErreur : Saisie impossible, veuillez choisir \"1\",\"2\" ou \"3\".\n");
+        } while (choix < 1 || choix > 3);
+        switch (choix) {
+            case 1:
+                //noir et blanc
+                //viewRechercheCouleur = new ViewRechercheCouleur(this.new ControllerRechercheCouleur());
+                //viewRechercherCouleur.run();
+                break;
+            case 2:
+                //couleur
+                //viewRechercheNoirBlanc = new ViewRechercheNoirBlanc(this.new ControllerRechercheNoirBlanc());
+                //viewRechercherNoirBlanc.run();
+                break;
+            case 3:
+                //retour
+                viewUtilisateur.run();
+                break;
+        }
     }
 }
