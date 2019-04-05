2ad6568b33203f3d649601c42eaea1cfe75bbd4a
Aurelien Montmejat <aurelien.montmejat@gmail.com>
2019-04-05 17:00:21 +0200

    Ajout du répertoire ressources/screens

--- InterfaceFX/src/sample/Controller.java
+++ InterfaceFX/src/sample/Controller.java
@@ -1,4 +1,18 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class Controller {
+
+    @FXML
+    protected void handlerButton(ActionEvent event) {
+        System.out.println("salut");
+    }
+
+    @FXML
+    protected void handlerComboBox(ActionEvent event) {
+        System.out.println(event.toString());
+    }
+
 }
