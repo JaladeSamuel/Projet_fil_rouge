5c166fe62987cf228b3e4a22be2bd780f21abcf5
JaladeSamuel <sam34440@hotmail.fr>
2019-04-06 23:43:58 +0200

    viewFX

---
+++ InterfaceFX/src/controller/MenuController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.KeyEvent;
+
+public class MenuController {
+    public void handleKeyInput(KeyEvent keyEvent) {
+    }
+
+    public void handleAboutAction(ActionEvent actionEvent) {
+    }
+}
